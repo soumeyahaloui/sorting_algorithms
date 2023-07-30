668ba245eeec7873d05254a64e1b787c8a22c1e7
soumeyahaloui <soumeyahaloui@gmail.com>
2023-07-29 18:25:32 -0700

    updated

---
+++ print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}

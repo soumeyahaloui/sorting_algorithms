e9f567e7de1e021f101887f749c0330c5a79a22b
soumeyahaloui <soumeyahaloui@gmail.com>
2023-07-29 18:39:04 -0700

    updated

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,33 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - Swaps two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	/* Update n1's next and n2's next if n2 is not NULL */
+	if ((*n1)->next != NULL)
+		(*n1)->next->prev = n2;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+
+	/* Update n1's prev and n2's prev */
+	n2->prev = (*n1)->prev;
+	(*n1)->prev = n2;
+
+	/* Update n1's next and n2's next */
+	(*n1)->next = n2->next;
+	n2->next = *n1;
+
+	/* Update head if n1 was the head */
+	if (n2->prev == NULL)
+		*h = n2;
+}
+
+/**
  * insertion_sort_list - Sorts a doubly linked list of integers in ascend order
  *                        using Insertion sort algorithm
  *
